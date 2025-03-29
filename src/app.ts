9b2d167df5687a8e1f4707afb7a6f6fdfef262f1
ipoblete <ignacio.poblete@evertecinc.com>
2025-03-28 22:04:03 -0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+
+export default app;
+
+if (require.main === module) {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
